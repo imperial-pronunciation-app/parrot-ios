eb1daeabb36fcf6d3a9c0930e5c0aba82a4606dc
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-01-22 10:45:42 +0000

    Initial Commit

---
+++ ParrotIosTests/ParrotIosTests.swift
@@ -0,0 +1,17 @@
+//
+//  ParrotIosTests.swift
+//  ParrotIosTests
+//
+//  Created by Pedro Sá Fontes on 22/01/2025.
+//
+
+import Testing
+@testable import ParrotIos
+
+struct ParrotIosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
