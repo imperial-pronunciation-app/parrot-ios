a4cfbf8836e0777c578a75873686fc8609e51118
James Watling <james.watling@gmail.com>
2025-01-22 23:04:11 +0000

    feat(post-recording): Added postData function to webService and created function in ViewModel to post an audioRecording to the recording endpoint

---
+++ ParrotIos/Features/Recording/Models/RecordingResponse.swift
@@ -0,0 +1,12 @@
+//
+//  RecordingResponse.swift
+//  ParrotIos
+//
+//  Created by James Watling on 22/01/2025.
+//
+
+struct RecordingResponse: Codable {
+    let recording_id: Int
+    let score: Int
+    let recording_phonemes: [Phoneme]
+}
--- ParrotIos/Services/WebService.swift
+++ ParrotIos/Services/WebService.swift
@@ -31,6 +31,36 @@ class WebService {
         
         return nil
     }
+    
+    func postData<T: Codable>(data: Data, toURL: String) async -> T? {
+        do {
+            // Create the request
+            guard let url = URL(string: toURL) else { throw NetworkError.badUrl }
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpBody = data
+            
+            let(responseData, response) = try await URLSession.shared.data(for: request)
+            guard let response = response as? HTTPURLResponse else { throw NetworkError.badResponse }
+            guard response.statusCode >= 200 && response.statusCode < 300 else { throw NetworkError.badStatus }
+            guard let decodedResponse = try? JSONDecoder().decode(T.self, from: responseData) else { throw NetworkError.failedToDecodeResponse }
+            
+            return decodedResponse
+        } catch NetworkError.badUrl {
+            print("Error creating the URL.")
+        } catch NetworkError.badResponse {
+            print("Bad response from the endpoint.")
+        } catch NetworkError.badStatus {
+            print("Did not receive a 2xx status from the response.")
+        } catch NetworkError.failedToDecodeResponse {
+            print("Failed to decode response into the given type.")
+        } catch {
+            print("An error occured when downloading the data.")
+        }
+        
+        return nil
+    }
 }
 
 enum NetworkError: Error {
