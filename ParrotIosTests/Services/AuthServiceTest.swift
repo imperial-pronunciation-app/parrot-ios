e005a18f142b2790a0a531adb6b380721f03b0fa
Euan Turner <euanoturner@gmail.com>
2025-02-11 12:22:51 +0000

    Set up service testing directory

---
+++ ParrotIosTests/Services/AuthServiceTest.swift
@@ -0,0 +1,16 @@
+//
+//  AuthServiceTest.swift
+//  ParrotIosTests
+//
+//  Created by et422 on 11/02/2025.
+//
+
+import Testing
+
+struct AuthServiceTest {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ ParrotIosTests/Services/ParrotApiServiceTest.swift
@@ -0,0 +1,16 @@
+//
+//  ParrotApiServiceTest.swift
+//  ParrotIosTests
+//
+//  Created by et422 on 11/02/2025.
+//
+
+import Testing
+
+struct ParrotApiServiceTest {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
