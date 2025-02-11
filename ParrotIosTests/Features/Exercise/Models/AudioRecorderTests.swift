f176ab13bc64c607d0f7b0ebd5e986e0a9ac0067
Euan Turner <euanoturner@gmail.com>
2025-02-11 12:34:27 +0000

    Set up feature testing files

---
+++ ParrotIosTests/Features/Exercise/Models/AudioRecorderTests.swift
@@ -0,0 +1,16 @@
+//
+//  AudioRecorderTests.swift
+//  ParrotIosTests
+//
+//  Created by et422 on 11/02/2025.
+//
+
+import Testing
+
+struct AudioRecorderTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
