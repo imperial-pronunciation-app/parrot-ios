fdc5f7eb5428f2c797284a6cd48e449337e21d5b
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-03-05 19:59:36 +0000

    fix: Refactor CurriculumView to add a pinned header with curriculum details

--- ParrotIos/Features/Unit/Views/CurriculumView.swift
+++ ParrotIos/Features/Unit/Views/CurriculumView.swift
@@ -19,33 +19,40 @@ struct CurriculumView: View {
                     } else if let error = viewModel.errorMessage {
                         UtilComponents.errorView(errorMessage: error)
                     } else {
-                        HStack {
-                            Image("en-US")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 32, height: 32)
-                            Text("Curriculum")
-                                .font(.title3)
-                                .fontWeight(.semibold)
-                            Spacer()
-                            HStack {
-                                Image(systemName: "flame.fill")
-                                    .foregroundColor(.orange)
-                                Text(String(viewModel.streaks()))
-                                    .font(.headline)
-                            }
-                        }
-                        .padding(.bottom, 16)
-
                         ForEach(Array(viewModel.curriculum!.units.enumerated()), id: \.element.id) { index, unit in
                             let isLast = index == viewModel.curriculum!.units.count - 1
                             let isNextCompleted = !isLast &&
-                                                  viewModel.curriculum!.units[index + 1].isCompleted
+                                viewModel.curriculum!.units[index + 1].isCompleted
                             UnitView(unit: unit, isLast: isLast, isNextCompleted: isNextCompleted)
                         }
                     }
                 }
                 .padding()
+            }
+            .safeAreaInset(edge: .top) {
+                // Pinned Header
+                HStack {
+                    Image("en-US")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 32, height: 32)
+                    
+                    Text("Curriculum")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Image(systemName: "flame.fill")
+                            .foregroundColor(.orange)
+                        Text("\(viewModel.streaks())")
+                            .font(.headline)
+                    }
+                }
+                .padding()
+                .background(.ultraThinMaterial)
+                .overlay(Divider(), alignment: .bottom)
             }
         }.onAppear {
             Task {
